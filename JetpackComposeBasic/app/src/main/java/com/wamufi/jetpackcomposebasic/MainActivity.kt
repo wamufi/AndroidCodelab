1d3649775ad01362e5f3fb01ff958d65517aa6c7
wamufi <1101001.ing@gmail.com>
2025-01-14 21:49:15 +0900

    Initial commit

---
+++ JetpackComposeBasic/app/src/main/java/com/wamufi/jetpackcomposebasic/MainActivity.kt
@@ -0,0 +1,145 @@
+package com.wamufi.jetpackcomposebasic
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.coerceAtLeast
+import androidx.compose.ui.unit.dp
+import com.wamufi.jetpackcomposebasic.ui.theme.JetpackComposeBasicTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackComposeBasicTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(name = "Android", modifier = Modifier.padding(innerPadding))
+//                }
+
+                MyApp(modifier = Modifier.fillMaxSize())
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Text("welcome to the Basic Codelab!")
+        Button(modifier = Modifier.padding(vertical = 24.dp), onClick = onContinueClicked) {
+            Text("Continue")
+        }
+    }
+}
+
+@Composable
+private fun Greetings(modifier: Modifier = Modifier, names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expanded = rememberSaveable { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(if (expanded.value) 48.dp else 0.dp, animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow))
+
+    Surface(color = MaterialTheme.colorScheme.primary, modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f).padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
+                Text(text = "Hello ")
+                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.ExtraBold))
+
+                if (expanded.value) {
+                    Text(
+                        text = ("Composem ipsum color sit lazy, " + "padding theme elit, sed do bouncy. ").repeat(4),
+                    )
+                }
+            }
+
+            IconButton(onClick = { expanded.value = !expanded.value }) {
+                Icon(imageVector = if (expanded.value) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore, contentDescription = if (expanded.value) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                })
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    JetpackComposeBasicTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    JetpackComposeBasicTheme {
+        OnboardingScreen(onContinueClicked = { })
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, uiMode = Configuration.UI_MODE_NIGHT_YES, name = "GreetingPreviewDark")
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GreetingsPreview() {
+    JetpackComposeBasicTheme {
+        Greetings()
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,11 @@
+# Android Codelabs
+## Jetpack Compose for Android Developers
+### Compose essentials
+#### Write your first Compose app
+JetpackComposeBasic
+https://developer.android.com/codelabs/jetpack-compose-basics
+#### Implement a real-world design
+BasicLayoutCodelabs
+https://developer.android.com/codelabs/jetpack-compose-layouts
+#### Get started with state
+https://developer.android.com/codelabs/jetpack-compose-state
